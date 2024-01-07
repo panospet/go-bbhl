6da39d074d1146b24445adbaf6fb1d81b4bacbb1
Panos Petropoulos <panospet@learnworlds.com>
2024-01-07 17:44:13 +0200

    add telegram simple msg cmd

---
+++ cmd/telegram-simple-msg/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	"github.com/caarlos0/env/v9"
+
+	"go-bbhl/uploaders/telegram"
+)
+
+type config struct {
+	TelegramToken string `env:"TELE_TOKEN,required"`
+	ChatId        int64  `env:"CHAT_ID,required"`
+}
+
+func main() {
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	upl, err := telegram.NewTelegramUploader(
+		cfg.TelegramToken,
+		cfg.ChatId,
+		// todo change to env var
+		"http://65.109.174.137:8081/bot%s/%s",
+	)
+	if err != nil {
+		log.Fatalf("Error creating uploader: %v", err)
+	}
+
+	log.Printf("uploading video to telegram channel...")
+	if err := upl.UploadVideo("gopher.mp4", "sample test"); err != nil {
+		log.Fatalf("Error uploading video: %v", err)
+	}
+}
